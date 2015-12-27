dbe79381140c1d4ecaef94485204ff7f3316f05a
Stefan Scherer <scherer_stefan@icloud.com>
2015-12-28 00:17:06 +0100

    Restart policy always

--- btsync/run.sh
+++ btsync/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --name Sync -p 8888:8888 -p 55555 -v /mnt/Public/share:/mnt/mounted_folders/share -v $(pwd)/sync:/mnt/sync --restart on-failure btsync
+docker run -d --name Sync -p 8888:8888 -p 55555 -v /mnt/Public/share:/mnt/mounted_folders/share -v $(pwd)/sync:/mnt/sync --restart always btsync
