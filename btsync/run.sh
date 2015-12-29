c56d49607de1f47d3817c92a090ed11b112cb1ec
Stefan Scherer <scherer_stefan@icloud.com>
2015-12-29 10:50:43 +0100

    Remove build and run scripts, use docker-compose

--- btsync/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -d --name Sync -p 8888:8888 -p 55555 -v /mnt/Public/share:/mnt/mounted_folders/share -v $(pwd)/sync:/mnt/sync --restart always btsync
--- forked-daapd/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir -p remote cache
-docker run -d --net=host --name daapd -p 3689:3689 -v /mnt/Public:/media -v $(pwd)/cache:/cache -v $(pwd)/remote:/remote --restart always forked-daapd
