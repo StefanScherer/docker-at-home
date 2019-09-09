c911e6f509a50546cdcf45f47dbcda24593479ac
Stefan Scherer <scherer_stefan@icloud.com>
2019-09-09 10:25:46 +0200

    Add PiHole

---
+++ pihole/start-systemd-resolved.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo systemctl enable systemd-resolved.service
+sudo service systemd-resolved start
---
+++ pihole/stop-systemd-resolved.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo systemctl disable systemd-resolved.service
+sudo service systemd-resolved stop
